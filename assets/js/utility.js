a2a6747bf761e2f02d99cde9d5b1a77b2ad870a5
Kai Wells <support@kaiwells.me>
2017-08-13 11:18:55 -0700

    randomIndex and removeAtIndex

---
+++ assets/js/utility.js
@@ -0,0 +1,22 @@
+function randomIndex(arr) {
+	if (arr.length < 1) {
+		return -1;
+	} else if (arr.length == 1) {
+		return 0;
+	} else {
+		return parseInt(Math.random()*arr.length);
+	}
+}
+
+function removeAtIndex(arr, i) {
+	var result = {removed: undefined, remaining: undefined};
+
+	if (i >= arr.length) {
+		result.remaining = arr;
+	} else {
+		result.removed = arr[i];
+		result.remaining = arr.slice(0, i).concat(arr.slice(i+1);)
+	}
+
+	return result;
+}
