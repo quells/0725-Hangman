cfc7827913eab0a2d777f60036e587d18ee13262
Kai Wells <support@kaiwells.me>
2017-08-17 15:42:46 -0700

    Refactor wheel & add letterboard

---
+++ assets/js/letterboard_draw.js
@@ -0,0 +1,49 @@
+var LetterBoard = function(charWidth, charHeight, padding) {
+	this.charWidth = charWidth;
+	this.charHeight = charHeight;
+	this.padding = padding;
+	this.bw = 2; // border width
+
+	this.bgCanvas = document.createElement("canvas");
+	this.bgCanvas.width = 14*charWidth + 2*padding + this.bw;
+	this.bgCanvas.height = 4*charHeight + 2*padding + this.bw;
+
+	var ctx = this.bgCanvas.getContext("2d");
+	ctx.fillStyle = "#6df"; // Light blue
+	ctx.fillRect(this.charWidth, 0, 12*this.charWidth+this.bw+2*this.padding, 4*this.charHeight+this.bw+2*this.padding);
+	ctx.fillRect(0, this.charHeight, 14*this.charWidth+this.bw+2*this.padding, 2*this.charHeight+this.bw+2*this.padding);
+	ctx.fillStyle = "#13f"; // Blue
+	ctx.fillRect(this.charWidth+0.5*this.padding, 0.5*this.padding, 12*this.charWidth+this.bw+this.padding, 4*this.charHeight+this.bw+this.padding);
+	ctx.fillRect(0.5*this.padding, this.charHeight+0.5*this.padding, 14*this.charWidth+this.bw+this.padding, 2*this.charHeight+this.bw+this.padding);
+	ctx.fillStyle = "#000"; // Black
+	ctx.fillRect(this.padding+this.charWidth, this.padding, 12*this.charWidth+this.bw, 4*this.charHeight+this.bw);
+	ctx.fillRect(this.padding, this.padding+this.charHeight, 14*this.charWidth+this.bw, 2*this.charHeight+this.bw);
+	ctx.fillStyle = "#184"; // Green
+	var x, y;
+	var w = this.charWidth-this.bw;
+	var h = this.charHeight-this.bw;
+	for (var j = 0; j < 4; j++) {
+		var nx = (j == 1 || j == 2) ? 14 : 12;
+		var ox = (j == 1 || j == 2) ? 0 : this.charWidth;
+		y = this.padding + this.charHeight*j + this.bw;
+		for (var i = 0; i < nx; i++) {
+			x = this.padding + ox + this.charWidth*i + this.bw;
+			ctx.fillRect(x, y, w, h);
+		}
+	}
+
+	this.fgCanvas = document.createElement("canvas");
+	this.fgCanvas.width = 14*charWidth;
+	this.fgCanvas.height = 4*charHeight;
+
+	this.canvas = document.createElement("canvas");
+	this.canvas.width = this.bgCanvas.width;
+	this.canvas.height = this.bgCanvas.height;
+
+	this.update = function() {
+		var ctx = this.canvas.getContext("2d");
+		ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		ctx.drawImage(this.bgCanvas, 0, 0);
+		// Draw letters from phrase
+	};
+};
--- assets/js/wheel_draw.js
+++ assets/js/wheel_draw.js
@@ -1,80 +1,81 @@
-function wheelText(ctx, number, deltaAngle) {
-	var offset = -80;
-	var dy = 12
-	ctx.rotate(deltaAngle);
-	for (var i = 0; i < number.length; i++) {
-		ctx.fillText(number.charAt(i), 0, offset+dy*i);
-	}
-}
+var Wheel = function(wheelPhysics) {
+	this.wheelPhysics = wheelPhysics;
 
-function newWheelCanvas() {
-	var wheelCanvas = document.createElement("canvas");
-	wheelCanvas.width = 200;
-	wheelCanvas.height = 200;
-	var wheelCtx = wheelCanvas.getContext("2d");
+	this.bgCanvas = document.createElement("canvas");
+	this.bgCanvas.width = 200;
+	this.bgCanvas.height = 200;
 
-	wheelCtx.translate(100, 100);
-	// wheelCtx.fillRect(-100, -100, 200, 200);
-
-	wheelCtx.fillStyle = "#666";
-	wheelCtx.beginPath();
-	wheelCtx.arc(0, 0, 100, 0, 2*Math.PI);
-	wheelCtx.fill();
+	var ctx = this.bgCanvas.getContext("2d");
+	ctx.translate(100, 100);
+	ctx.fillStyle = "#666";
+	ctx.beginPath();
+	ctx.arc(0, 0, 100, 0, 2*Math.PI);
+	ctx.fill();
 
 	var colors = ["#fdb827", "#f5821f", "#e03a3e", "#963d97", "#009ddc", "#57bb46", "#000", "#57bb46", "#009ddc", "#963d97", "#e03a3e", "#f5821f"];
 	for (var i = 0; i < 12; i++) {
-		wheelCtx.fillStyle = colors[i];
-		wheelCtx.beginPath();
-		wheelCtx.arc(0, 0, 98, (-0.5-1/12)*Math.PI, (-0.5+1/12)*Math.PI);
-		wheelCtx.lineTo(0, 0);
-		wheelCtx.closePath();
-		wheelCtx.fill();
-		wheelCtx.rotate(Math.PI/6);
+		ctx.fillStyle = colors[i];
+		ctx.beginPath();
+		ctx.arc(0, 0, 98, (-0.5-1/12)*Math.PI, (-0.5+1/12)*Math.PI);
+		ctx.lineTo(0, 0);
+		ctx.closePath();
+		ctx.fill();
+		ctx.rotate(Math.PI/6);
 	}
 
-	wheelCtx.fillStyle = "#fff";
-	wheelCtx.font = "12px Helvetica";
-	wheelCtx.textAlign = "center";
+	ctx.fillStyle = "#fff";
+	ctx.font = "12px sans-serif";
+	ctx.textAlign = "center";
 
-	wheelText(wheelCtx, "1000", 0);
-	wheelText(wheelCtx, "750", Math.PI/6);
-	wheelText(wheelCtx, "500", Math.PI/6);
-	wheelText(wheelCtx, "250", Math.PI/6);
-	wheelText(wheelCtx, "100", Math.PI/6);
-	wheelText(wheelCtx, "900", Math.PI/6);
-	wheelText(wheelCtx, "900", Math.PI/3);
-	wheelText(wheelCtx, "100", Math.PI/6);
-	wheelText(wheelCtx, "250", Math.PI/6);
-	wheelText(wheelCtx, "500", Math.PI/6);
-	wheelText(wheelCtx, "750", Math.PI/6);
-	wheelCtx.rotate(Math.PI*2/3);
-	wheelCtx.font = "10px Helvetica";
-	wheelCtx.fillText("BANKRUPT", 66, 3);
+	var numbers = ["1000", "750", "500", "250", "100", "900", "900", "100", "250", "500", "750"];
+	var offset = -80;
+	var dy = 12;
+	for (var i = 0; i < 11; i++) {
+		var deltaAngle = Math.PI/6;
+		if (i == 0) { deltaAngle = 0; }
+		if (i == 6) { deltaAngle = Math.PI/3; }
+		ctx.rotate(deltaAngle);
+		var number = numbers[i];
+		for (var j = 0; j < number.length; j++) {
+			ctx.fillText(number.charAt(j), 0, offset+dy*j);
+		}
+	}
+	ctx.rotate(Math.PI*2/3);
+	ctx.font = "10px sans-serif";
+	ctx.fillText("BANKRUPT", 66, 3);
 
-	wheelCtx.fillStyle = "#999";
-	wheelCtx.beginPath();
-	wheelCtx.arc(0, 0, 35, 0, 2*Math.PI);
-	wheelCtx.fill();
+	ctx.fillStyle = "#999";
+	ctx.beginPath();
+	ctx.arc(0, 0, 35, 0, 2*Math.PI);
+	ctx.fill();
 
-	return wheelCanvas
-}
+	this.fgCanvas = document.createElement("canvas");
+	this.fgCanvas.width = 250;
+	this.fgCanvas.height = 250;
+	ctx = this.fgCanvas.getContext("2d");
 
-function drawWheel(ctx, wheel, angle) {
-	var x = -wheel.width/2;
-	var y = -wheel.height/2;
-	ctx.rotate(angle);
-	ctx.drawImage(wheel, x, y);
-	ctx.rotate(-angle);
-
-	// Draw arrow
 	ctx.fillStyle = "#ccc";
 	ctx.strokeStyle = "#333";
 	ctx.lineWidth = 1;
 	ctx.beginPath();
-	ctx.moveTo(x+110, y-5);
-	ctx.lineTo(x+100, y+15);
-	ctx.lineTo(x+ 90, y-5);
+	ctx.moveTo(135, 15);
+	ctx.lineTo(125, 35);
+	ctx.lineTo(115, 15);
 	ctx.closePath();
 	ctx.fill();
 	ctx.stroke();
-}
+
+	this.canvas = document.createElement("canvas");
+	this.canvas.width = 250;
+	this.canvas.height = 250;
+
+	this.update = function() {
+		this.canvas.width = this.canvas.width;
+		var ctx = this.canvas.getContext("2d");
+		ctx.translate(this.canvas.width/2, this.canvas.width/2);
+		ctx.rotate(this.wheelPhysics.theta);
+		ctx.drawImage(this.bgCanvas, -this.bgCanvas.width/2, -this.bgCanvas.width/2);
+		ctx.rotate(-this.wheelPhysics.theta);
+		ctx.drawImage(this.fgCanvas, -125, -125);
+	};
+};
